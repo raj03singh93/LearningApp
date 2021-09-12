a0c58d4d8faee2107caa199bd063b856b1868bc8
Raj <raj03.singh93@gmail.com>
2021-09-12 20:02:20 +0530

    added Initial controllers

--- LearningApp.Facade.API/Startup.cs
+++ LearningApp.Facade.API/Startup.cs
@@ -16,6 +16,7 @@ namespace LearningApp.Facade.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,11 +26,14 @@ namespace LearningApp.Facade.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
